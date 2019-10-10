bacbfac470d3e5cd41c106889c1e9c584149b43a
mja94 <monica.ayala.1@etsmtl.net>
2019-10-10 14:47:23 -0400

    add morning review ui

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TasksPage } from './tasks.page';
+import { ComponentsModule } from './../components/components.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ComponentsModule,
     RouterModule.forChild([{ path: '', component: TasksPage }])
   ],
   declarations: [TasksPage]
 })
-export class TasksPageModule {}
+export class TasksPageModule { }
