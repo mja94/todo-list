f51784644da9fb1b17b7a02f176136a8821c7038
mja94 <monica.ayala.1@etsmtl.net>
2019-10-11 17:22:23 -0400

    dynamically add item in lists

--- src/app/morning-review/morning-review.page.ts
+++ src/app/morning-review/morning-review.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+
 
 @Component({
   selector: 'app-morning-review',
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./morning-review.page.scss'],
 })
 export class MorningReviewPage implements OnInit {
-
+  inputTask: string;
+  inputGoal: string;
+  focusInput: any;
   constructor() { }
+  taskList = [];
+  goalList = [];
+  gratefulList = [];
+  affirmation = '';
 
   ngOnInit() {
+  }
+
+
+  addItem() {
+
+    if (this.focusInput.value !== undefined) {
+      const item = this.focusInput.value;
+      console.log(this.focusInput.value);
+      if (item.length && item.trim() !== '') {
+
+        if (this.focusInput.name === 'task') {
+          this.taskList.push(item);
+        }
+        if (this.focusInput.name === 'goal') {
+          this.goalList.push(item);
+        }
+        if (this.focusInput.name === 'grateful') {
+          this.gratefulList.push(item);
+        }
+        if (this.focusInput.name === 'affirmation') {
+          this.affirmation = item;
+        }
+      }
+      this.focusInput.value = '';
+    }
+  }
+
+  deleteItem() {
+    console.log('delete item');
+  }
+
+  change(event) {
+    console.log(event.target.name);
+    this.focusInput = event.target;
+
+  }
+
+  changeModel(event) {
+    console.log('Model change', event);
   }
 
 }
