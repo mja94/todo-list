5be3f4344f766aa15860323b1b976ce391d49b5d
mja94 <monica.ayala.1@etsmtl.net>
2019-10-13 00:01:46 -0400

    link with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import * as firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB0F9DCV7AJM5qlMKxsfkRBfhAXiIVpEaU',
+  authDomain: 'todoapp-ff5c2.firebaseapp.com',
+  databaseURL: 'https://todoapp-ff5c2.firebaseio.com',
+  projectId: 'todoapp-ff5c2',
+  storageBucket: 'todoapp-ff5c2.appspot.com',
+  messagingSenderId: '859606420504',
+  appId: '1:859606420504:web:8c56bcd0704efde41b476d',
+  measurementId: 'G-8WBRPWWFMY'
+});
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,4 +32,4 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
